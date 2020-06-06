d8a1ea7658a86c091664f53cf6fad774fd648cfa
David Rosin <davidprosin@gmail.com>
2020-06-06 17:26:22 +0200

    Refactor import of preprocess fct.

--- src/prediction.py
+++ src/prediction.py
@@ -1,5 +1,5 @@
-import importlib
 import numpy as np
+from keras.applications.mobilenet_v2 import preprocess_input
 from imageatm.components import Evaluation
 from src.handlers.image_helper import process_webcam_image
 
@@ -9,13 +9,10 @@ class Predictor:
     def __init__(self, image_dir, job_dir):
         e = Evaluation(image_dir=image_dir, job_dir=job_dir)
         self.model = e.best_model
-        self.preprocess_mobilenet = importlib.import_module(
-            'tensorflow.keras.applications.mobilenet_v2'
-        ).preprocess_input
 
     def predict(self, webcam_image):
         image = process_webcam_image(webcam_image)
-        image = self.preprocess_mobilenet(image)
+        image = preprocess_input(image)
         x = np.expand_dims(image, axis=0)
         result = self.model.predict(x)
         # TODO: convert result to dict referencing class names
@@ -26,13 +23,10 @@ class Presence_Predictor:
     def __init__(self, presence_image_dir, presence_job_dir):
         e = Evaluation(image_dir=presence_image_dir, job_dir=presence_job_dir)
         self.model = e.best_model
-        self.preprocess_mobilenet = importlib.import_module(
-            'tensorflow.keras.applications.mobilenet_v2'
-        ).preprocess_input
 
     def predict(self, webcam_image):
         image = process_webcam_image(webcam_image)
-        image = self.preprocess_mobilenet(image)
+        image = preprocess_input(image)
         x = np.expand_dims(image, axis=0)
         result = self.model.predict(x)
         # TODO: convert result to dict referencing class names
